9546a2a17741d07e349eee8194438a3346b6da13
Vitor Tominaga Mussatto <vmussatto@bol.com>
2020-08-15 08:38:43 +0200

    adding workers

--- src/main/kotlin/mussatto/pocAsyncChannel/ChannelListenerBean.kt
+++ src/main/kotlin/mussatto/pocAsyncChannel/ChannelListenerBean.kt
@@ -5,14 +5,15 @@ import kotlinx.coroutines.launch
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
 
-@Component
-class ChannelListenerBean(val channelComponent: ChannelComponent): InitializingBean {
-    override fun afterPropertiesSet() {
-        GlobalScope.launch{
-            for(message in channelComponent.channelMessage){
-                Thread.sleep(5000)
-                println("The message is $message")
-            }
-        }
-    }
-}
+//@Component
+//class ChannelListenerBean(val channelComponent: ChannelComponent) : InitializingBean {
+//    override fun afterPropertiesSet() {
+//        GlobalScope.launch {
+//            for (message in channelComponent.channelMessage) {
+//                // THIS IS SLOOOOOW, that is why we need an async process!
+//                Thread.sleep(5000)
+//                println("The message is $message")
+//            }
+//        }
+//    }
+//}
--- src/main/kotlin/mussatto/pocAsyncChannel/TestController.kt
+++ src/main/kotlin/mussatto/pocAsyncChannel/TestController.kt
@@ -4,18 +4,19 @@ import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class TestController(val channelComponent: ChannelComponent) {
 
     @GetMapping("/async")
     fun ShouldProcessAsync(@RequestParam message: String): String {
-
+        val myId = UUID.randomUUID().toString();
         runBlocking {
-            channelComponent.channelMessage.send("Sending Message!")
+            channelComponent.channelMessage.send("Sending Message! id=$myId")
             println("Message sent!")
         }
 
-        return "Enqueued!"
+        return myId;
     }
 }
